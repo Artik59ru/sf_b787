bd633c3152c32a3e908b44fc3ef3fa1eed709c32
Artem Gilev <agilev90@gmail.com>
2022-04-07 09:36:47 +0000

    Edited start.sh

--- start.sh
+++ start.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-docker run --mount type=bind,source="$(pwd)"/,target=/opt/test/  --rm alpine_wget:v1 $1/favicon.ico
+docker run --mount type=bind,source="$(pwd)"/,target=/opt/test/ --rm alpine_wget:v1 $1/favicon.ico
